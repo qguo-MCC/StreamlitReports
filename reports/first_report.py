184a4356aea8c346973203b89e9f40649ac514c2
qguo-MCC <qguo@mcc.ca>
2023-03-07 12:26:59 -0500

    sidebar

--- reports/first_report.py
+++ reports/first_report.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.title('First Report')
 st.text('Experiment 1.')
 st.text('Experiment 2.')
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
