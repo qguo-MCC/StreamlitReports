2db85499ccc6d494964f230a691a6cf4c09023d8
qguo-MCC <qguo@mcc.ca>
2023-03-07 12:43:47 -0500

    experiment secure connection

--- reports/first_report.py
+++ reports/first_report.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.title('First Report')
 st.text('Experiment 1.')
 st.text('Experiment 2.')
@@ -6,3 +7,4 @@ add_selectbox = st.sidebar.selectbox(
     "How would you like to be contacted?",
     ("Email", "Home phone", "Mobile phone")
 )
+st.dataframe(pd.read_csv(r'\\MCC-DFS01\Mcc.Tdm.Marking\PROD\2023-January\09 PAS\streamlit_data/try1.csv'))
