6b1ed5ed9842393abc788ba1df92416ca1be7640
qguo-MCC <qguo@mcc.ca>
2023-03-14 17:29:04 -0400

    connect with snowflake

--- reports/first_report.py
+++ reports/first_report.py
@@ -2,9 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+import snowflake.connector
+@st.cache_resource
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection()
 st.title('First Report')
 st.text('Experiment 1.')
 st.text('Experiment 2.')
+cur = conn.cursor()
+q = cur.execute('SELECT * FROM testschema_mg.truth_table')
+df = q.fetch_pandas_all()
+st.dataframe(df)
+"""
 with st.sidebar:
     with st.echo():
         st.write("This code will be printed to the sidebar.")
@@ -17,3 +30,4 @@ with st.sidebar:
 df = pd.read_csv('data/try1.csv')
 st.dataframe(df)
 #pd.read_csv(r'\\MCC-DFS01\Mcc.Tdm.Marking\PROD\2023-January\09 PAS\streamlit_data/try1.csv')
+"""
