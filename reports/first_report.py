75effc3743a2301a5913ab7ccd3178d4aff7fb13
qguo-MCC <qguo@mcc.ca>
2023-03-14 17:38:40 -0400

    connect with snowflake

--- reports/first_report.py
+++ reports/first_report.py
@@ -17,17 +17,9 @@ cur = conn.cursor()
 q = cur.execute('SELECT * FROM testschema_mg.truth_table')
 df = q.fetch_pandas_all()
 st.dataframe(df)
-"""
-with st.sidebar:
-    with st.echo():
-        st.write("This code will be printed to the sidebar.")
 
-    with st.spinner("Loading..."):
-        time.sleep(5)
-    st.success("Done!")
-#df = pd.DataFrame(np.random.randn(50, 20), columns=('col %d' % i for i in range(20)))
-#df.to_csv('data/try1.csv', index=False)
-df = pd.read_csv('data/try1.csv')
-st.dataframe(df)
-#pd.read_csv(r'\\MCC-DFS01\Mcc.Tdm.Marking\PROD\2023-January\09 PAS\streamlit_data/try1.csv')
-"""
+with st.sidebar:
+    st.header("Truths")
+    st.write("Truth 1: jn is a gtw.")
+    st.write("Truth2: wt is a gtw.")
+
