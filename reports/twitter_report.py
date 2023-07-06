fcc1bd14aaba1c7a14106071cd9faff5121fe93a
qguo-MCC <qguo@mcc.ca>
2023-07-06 01:57:15 -0400

    update streamlit sqlite connection try session.connection()

--- reports/twitter_report.py
+++ reports/twitter_report.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from src.utilities.save_load_python_object import load_obj
 from src.utilities.social_network_utilities import calculate_centrality, get_leader_tweets
 import pandas as pd
-#from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 
 st.set_page_config(layout="wide")
 
@@ -26,13 +26,19 @@ cluster_option = st.sidebar.selectbox(
     'select cluster',
     tuple(clusters))
 fig = load_obj(root.joinpath(f'{query_option}_{edge_type_option}_plotly.fig'))
-conn = st.experimental_connection('data_db', type='sql')
+engine = st.experimental_connection('data_db', type='sql').session.connection(close_with_result=False)
 if cluster_option == 'all':
     st.plotly_chart(fig, use_container_width=True)
+    user_class = pd.read_sql(f'SELECT Medical_professional, Advocate_Activist, Educator, Researcher, Job_Posting, organizations, Government, Miscellaneous FROM user_descriptions', engine)\
+        .replace('None', None)\
+        .dropna()\
+        .astype(int)
     st.subheader('GPT4 summary of leader tweets')
     summary = pd.read_excel('data/GPT4summaries.xlsx', engine='openpyxl', sheet_name=f'{query_option}_{edge_type_option}')
     summary[['Theme', 'Summary']] = summary['Themes'].str.split(': ', expand = True)
     st.dataframe(summary[['Theme', 'Summary']], use_container_width=True, hide_index=True)
+    st.subheader('User Classification')
+    st.dataframe(user_class.sum().reset_index().transpose(), use_container_width=True, hide_index=True)
     st.subheader('Top 10 leaders based on indegree centrality')
     st.dataframe(calculate_centrality(nx.in_degree_centrality, 'Indegree', G), use_container_width=True)
     st.subheader('Top 10 leaders based on outdegree centrality')
@@ -50,10 +56,10 @@ else:
     st.plotly_chart(fig, use_container_width=True)
     cmembers = [node for node in G.nodes if G.nodes[node]['cluster'] == int(cluster_option)]
 
-    users = pd.read_sql(f'SELECT user_id, username FROM authors WHERE username in {str(tuple(cmembers)).replace(",)", ")")}', conn.session.connection(close_with_result=True))
+    users = pd.read_sql(f'SELECT user_id, username FROM authors WHERE username in {str(tuple(cmembers)).replace(",)", ")")}', engine)
 
     cids = str(tuple(users['user_id'].to_list())).replace(",)", ")")
-    user_class = pd.read_sql(f'SELECT Medical_professional, Advocate_Activist, Educator, Researcher, Job_Posting, organizations, Government, Miscellaneous FROM user_descriptions WHERE user_id in {cids}', conn.session.connection(close_with_result=True))\
+    user_class = pd.read_sql(f'SELECT Medical_professional, Advocate_Activist, Educator, Researcher, Job_Posting, organizations, Government, Miscellaneous FROM user_descriptions WHERE user_id in {cids}', engine)\
         .replace('None', None)\
         .dropna()\
         .astype(int)
@@ -71,7 +77,7 @@ else:
     st.subheader('Cluster Leader')
     leader = centralities.index[0]
     st.write(leader)
-    tweets = pd.DataFrame({'Tweets': get_leader_tweets(leader, queries, conn.session.connection(close_with_result=True))})
+    tweets = pd.DataFrame({'Tweets': get_leader_tweets(leader, queries, engine)})
     st.subheader(f'Tweets by or about {leader}')
     st.dataframe(tweets, use_container_width = True, hide_index=True, column_config=None)
 
