8d7d682b515181bd207e06e22b490437eb233bee
qguo-MCC <qguo@mcc.ca>
2023-07-06 03:38:09 -0400

    change to load csv file rather using sql database

--- reports/twitter_report_csv.py
+++ reports/twitter_report_csv.py
@@ -26,14 +26,15 @@ cluster_option = st.sidebar.selectbox(
     'select cluster',
     tuple(clusters))
 fig = load_obj(root.joinpath(f'{query_option}_{edge_type_option}_plotly.fig'))
-var_names = ['Medical_professional', 'Advocate_Activist', 'Educator', 'Researcher', 'Job_Posting', 'organizations', 'Government', 'Miscellaneous']
-user_class = pd.read_csv('data/user_descriptions.csv')[var_names] \
-    .replace('None', None) \
-    .dropna() \
-    .astype(int)
+
 if cluster_option == 'all':
     st.plotly_chart(fig, use_container_width=True)
-
+    var_names = ['Medical_professional', 'Advocate_Activist', 'Educator', 'Researcher', 'Job_Posting', 'organizations',
+                 'Government', 'Miscellaneous']
+    user_class = pd.read_csv('data/user_descriptions.csv')[var_names] \
+        .replace('None', None) \
+        .dropna() \
+        .astype(int)
 
     st.subheader('GPT4 summary of leader tweets')
     summary = pd.read_excel('data/GPT4summaries.xlsx', engine='openpyxl', sheet_name=f'{query_option}_{edge_type_option}')
@@ -60,12 +61,19 @@ else:
 
     users = pd.read_csv('data/users.csv')
     users = users.loc[users['username'].isin(cmembers)]
-    cids = str(tuple(users['user_id'].to_list())).replace(",)", ")")
+    cids = users['user_id'].to_list()
     st.subheader('Cluster Title')
     st.write('To be implement later if needed.')
     st.subheader('Cluster Summary')
     st.write('To be implement later if needed.')
     st.subheader('Cluster Member Classification')
+    var_names = ['Medical_professional', 'Advocate_Activist', 'Educator', 'Researcher', 'Job_Posting', 'organizations',
+                 'Government', 'Miscellaneous']
+    user_class = pd.read_csv('data/user_descriptions.csv')
+    user_class = user_class.loc[user_class['username'].isin(cmembers), var_names]\
+        .replace('None', None) \
+        .dropna() \
+        .astype(int)
     st.dataframe(user_class.sum().reset_index().transpose(), use_container_width=True, hide_index=True)
     centralities = nx.in_degree_centrality(G)
     centralities = pd.Series(centralities)
@@ -77,8 +85,8 @@ else:
     st.write(leader)
     tweets = pd.read_csv('data/tweets.csv')
     tweets = tweets.loc[tweets['query_number'].isin(queries), ['text', 'user']].rename(columns={'user': 'user_id'})
-
-    tweets = pd.DataFrame({'Tweets': get_leader_tweets_csv(leader, tweets, users.loc[users['username']==leader, 'user_id'].values[0])})
+    user_id = users.loc[users['username']==leader, 'user_id'].values[0]
+    tweets = pd.DataFrame({'Tweets': get_leader_tweets_csv(leader, tweets, user_id)})
     st.subheader(f'Tweets by or about {leader}')
     st.dataframe(tweets, use_container_width = True, hide_index=True, column_config=None)
 
